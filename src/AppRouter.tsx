2bade148cb1d809e1b2a7276fc9f89a86d8fc349
sebastian <sebastian.dev0708gmail.com>
2025-08-31 07:04:05 -0500

    Default router

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,5 +1,5 @@
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Ejercicio1 } from './ejercicios/Ejercicio1'
 import { Ejercicio3 } from './ejercicios/Ejercicio3'
 import { Ejercicio2 } from './ejercicios/Ejercicio2'
@@ -7,6 +7,7 @@ import { Ejercicio2 } from './ejercicios/Ejercicio2'
 export const AppRouter = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/ejercicio_1" />} />
       <Route path="/ejercicio_1" element={<Ejercicio1 />} />
       <Route path="/ejercicio_2" element={<Ejercicio2 />} />
       <Route path="/ejercicio_3" element={<Ejercicio3 />} />
